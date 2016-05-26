9f14a060c7af118143848440ec599b45289a1c8e
Tiago Manuel Fragoso Conceição <tiago_caza@hotmail.com>
2016-05-26 13:56:04 +0100

    Added bar code to detail page and track page

--- Aliexpress Tracking QR-Barcode.user.js
+++ Aliexpress Tracking QR-Barcode.user.js
@@ -10,23 +10,25 @@
 // @homepage     https://github.com/sn4k3/Aliexpress-Tracking-QRBarcode
 // @supportURL   https://github.com/sn4k3/Aliexpress-Tracking-QRBarcode/issues
 // @match        http*://*trade.aliexpress.com/order_detail.htm*
+// @match        http*://*track.aliexpress.com/logisticsdetail.htm*
 // @grant        none
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/lrsjng.jquery-qrcode/0.12.0/jquery.qrcode.js
+// @require      https://cdn.jsdelivr.net/jsbarcode/3.3.5/barcodes/JsBarcode.code128.min.js
 // ==/UserScript==
-
-// https://cdn.jsdelivr.net/jsbarcode/3.3.5/barcodes/JsBarcode.code128.min.js BACKUP
 
 (function() {
     'use strict';
     $(document).ready(function() {
         var i = 0;
         var trackingNumbers = [];
+        
+        // trade.aliexpress.com/order_detail.htm
         $('.shipping-bd .no').each(function() {
-            i++;
             var trackingNumber = $(this).text().trim();
             if(trackingNumber)
             {
+                i++;
                 trackingNumbers.push(trackingNumber);
                 //$(this).html('<svg id="trackingBarcode'+i+'" class="trackingBarcode" jsbarcode-format="CODE128A" jsbarcode-value="'+trackingNumber+'" jsbarcode-textmargin="0" jsbarcode-fontoptions="bold" jsbarcode-width="1"></svg>');
                 $(this).html(trackingNumber+'<a href="http://www.17track.net/en/track?nums='+trackingNumber+'" target="_blank"><div id="trackingQRCode'+i+'" class="trackingQRCode"></div></a>');
@@ -36,9 +38,26 @@
                     //'label': trackingNumber,
                     "text": trackingNumber
                 });
+                
+                if(i == 1)
+                {
+                    var tableID = $(this).parent().attr('id');
+                    $('<div align="center"><a href="http://www.17track.net/en/track?nums='+trackingNumber+'" target="_blank"><svg id="trackingBarcode'+i+'" class="trackingBarcode" jsbarcode-format="CODE128A" jsbarcode-value="'+trackingNumber+'" jsbarcode-textmargin="0" jsbarcode-fontoptions="bold" jsbarcode-width="2"></svg></a></div>').insertAfter( "#"+tableID+" .list-box");
+                    JsBarcode(".trackingBarcode").init();
+                }
             }
             //console.log(trackingNumber);
         });
+        
+        // track.aliexpress.com/logisticsdetail.htm
+        var trackingNumber = $('.consignment-detail-content .detail-row:first .item.msg:first').text();
+        if(trackingNumber)
+        {
+            i++;
+            trackingNumbers.push(trackingNumber);
+            $('<div align="center"><a href="http://www.17track.net/en/track?nums='+trackingNumber+'" target="_blank"><svg id="trackingBarcode'+i+'" class="trackingBarcode" jsbarcode-format="CODE128A" jsbarcode-value="'+trackingNumber+'" jsbarcode-textmargin="0" jsbarcode-fontoptions="bold" jsbarcode-width="2"></svg></a></div>').insertAfter( ".consignment-detail-content:first");
+            JsBarcode(".trackingBarcode").init();
+        }
 
         if(trackingNumbers.length > 0)
         {
